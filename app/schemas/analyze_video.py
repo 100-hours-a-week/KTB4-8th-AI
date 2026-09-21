81dd533515015605ffd7560108ba19a85441f4a3
kimSR0916 <amy10305@naver.com>
2026-09-21 16:38:28 +0900

    category str -> literal 변경

--- app/schemas/analyze_video.py
+++ app/schemas/analyze_video.py
@@ -1,5 +1,5 @@
 from datetime import date
-from typing import Optional
+from typing import Literal, Optional
 from pydantic import BaseModel, Field
 
 from app.schemas.common import APIResponse
@@ -12,7 +12,7 @@ class AnalyzeVideoRequest(BaseModel):
 class AnalyzeVideoData(BaseModel):
     place_name : Optional[str] = Field(default = None, description = "영상에 나온 장소 이름")
     region : Optional[str] = Field(default = None, description = "영상에 나온 장소 위치")
-    category : str = Field(description = "장소의 카테고리, 비슷한것끼리 묶임 (ex. 카페)") #돌려보고 정규화 맘에 들게 이뤄지지 않으면 Literal 사용)
+    category : Literal["카페","팝업","전시","맛집","명소", "기타"] = Field(description = "카페, 팝업, 전시, 맛집, 명소 어디에도 해당하지 않으면 기타로 분류한다")
     event_start_date : Optional[date] = Field(default = None, description = "장소 개최일, 팝업/전시 카테고리에서만 값을 채움")
     event_end_date : Optional[date] = Field(default = None, description = "장소 마감일, 팝업/전시 카테고리에서만 값을 채움")
     confidence : float = Field(description = "추출된 장소에 대한 신뢰도", ge = 0.0, le = 1.0)
